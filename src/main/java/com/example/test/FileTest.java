e367a4c2fc46c7bb9f14e382cbf57f7a8c0fb686
yangyang.jiang <378070113@qq.com>
2020-07-21 08:25:49 +0800

    java8 stream学习

---
+++ src/main/java/com/example/test/FileTest.java
@@ -0,0 +1,16 @@
+package com.example.test;
+
+/**
+ * @author yangyang.jiang
+ * @Description:
+ * @create 2020/7/17
+ * @time 3:48 下午
+ */
+public class FileTest {
+
+    public static void main(String[] args) {
+        System.out.println("🙂".length());
+        System.out.println("🙂".getBytes().length);
+        System.out.println("号".getBytes().length);
+    }
+}
